69cd0f7916b81460bd1aebbe7edb900f9d48fb31
MohKatami <mohkatami050@gmail.com>
2022-10-22 12:08:00 +0700

    first commit

---
+++ style/js/style.js
@@ -0,0 +1,158 @@
+function sweetAlert() {
+  swal({
+    title: "Thanks!",
+    text: "You Order This Menu",
+    icon: "success",
+    button: "OK",
+  });
+}
+
+// icon changer
+// fav
+function changeImage1() {
+  let displayFav = document.getElementById("fav");
+  if (displayFav.src.match("/assets/icon/heart.png")) {
+    displayFav.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav.src = "/assets/icon/heart.png";
+  }
+}
+function changeFav1() {
+  let displayFav1 = document.getElementById("fav1");
+  if (displayFav1.src.match("/assets/icon/heart.png")) {
+    displayFav1.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav1.src = "/assets/icon/heart.png";
+  }
+}
+function changeFav2() {
+  let displayFav2 = document.getElementById("fav2");
+  if (displayFav2.src.match("/assets/icon/heart.png")) {
+    displayFav2.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav2.src = "/assets/icon/heart.png";
+  }
+}
+function changeFav3() {
+  let displayFav3 = document.getElementById("fav3");
+  if (displayFav3.src.match("/assets/icon/heart.png")) {
+    displayFav3.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav3.src = "/assets/icon/heart.png";
+  }
+}
+function changeFav4() {
+  let displayFav4 = document.getElementById("fav4");
+  if (displayFav4.src.match("/assets/icon/heart.png")) {
+    displayFav4.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav4.src = "/assets/icon/heart.png";
+  }
+}
+function changeFav5() {
+  let displayFav5 = document.getElementById("fav5");
+  if (displayFav5.src.match("/assets/icon/heart.png")) {
+    displayFav5.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav5.src = "/assets/icon/heart.png";
+  }
+}
+function changeFav6() {
+  let displayFav6 = document.getElementById("fav6");
+  if (displayFav6.src.match("/assets/icon/heart.png")) {
+    displayFav6.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav6.src = "/assets/icon/heart.png";
+  }
+}
+function changeFav7() {
+  let displayFav7 = document.getElementById("fav7");
+  if (displayFav7.src.match("/assets/icon/heart.png")) {
+    displayFav7.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav7.src = "/assets/icon/heart.png";
+  }
+}
+function changeFav8() {
+  let displayFav8 = document.getElementById("fav8");
+  if (displayFav8.src.match("/assets/icon/heart.png")) {
+    displayFav8.src = "/assets/icon/heart-1.png";
+  } else {
+    displayFav8.src = "/assets/icon/heart.png";
+  }
+}
+
+// rating
+
+function changeStar() {
+  let star = document.getElementById("rating");
+  if (star.src.match("/assets/icon/star.png")) {
+    star.src = "/assets/icon/star-1.png";
+  } else {
+    star.src = "/assets/icon/star.png";
+  }
+}
+function changeStar1() {
+  let star1 = document.getElementById("rating1");
+  if (star1.src.match("/assets/icon/star.png")) {
+    star1.src = "/assets/icon/star-1.png";
+  } else {
+    star1.src = "/assets/icon/star.png";
+  }
+}
+function changeStar2() {
+  let star2 = document.getElementById("rating2");
+  if (star2.src.match("/assets/icon/star.png")) {
+    star2.src = "/assets/icon/star-1.png";
+  } else {
+    star2.src = "/assets/icon/star.png";
+  }
+}
+function changeStar3() {
+  let star3 = document.getElementById("rating3");
+  if (star3.src.match("/assets/icon/star.png")) {
+    star3.src = "/assets/icon/star-1.png";
+  } else {
+    star3.src = "/assets/icon/star.png";
+  }
+}
+function changeStar4() {
+  let star4 = document.getElementById("rating4");
+  if (star4.src.match("/assets/icon/star.png")) {
+    star4.src = "/assets/icon/star-1.png";
+  } else {
+    star4.src = "/assets/icon/star.png";
+  }
+}
+function changeStar5() {
+  let star5 = document.getElementById("rating5");
+  if (star5.src.match("/assets/icon/star.png")) {
+    star5.src = "/assets/icon/star-1.png";
+  } else {
+    star5.src = "/assets/icon/star.png";
+  }
+}
+function changeStar6() {
+  let star6 = document.getElementById("rating6");
+  if (star6.src.match("/assets/icon/star.png")) {
+    star6.src = "/assets/icon/star-1.png";
+  } else {
+    star6.src = "/assets/icon/star.png";
+  }
+}
+function changeStar7() {
+  let star7 = document.getElementById("rating7");
+  if (star7.src.match("/assets/icon/star.png")) {
+    star7.src = "/assets/icon/star-1.png";
+  } else {
+    star7.src = "/assets/icon/star.png";
+  }
+}
+function changeStar8() {
+  let star8 = document.getElementById("rating8");
+  if (star8.src.match("/assets/icon/star.png")) {
+    star8.src = "/assets/icon/star-1.png";
+  } else {
+    star8.src = "/assets/icon/star.png";
+  }
+}
